45d5052e4796ab6dcdb03e41222a54a07e44be4c
Iulian Ilie Nemedi <iilie@axway.com>
2023-03-27 23:02:20 +0300

    INT - Adding producer-consumer-storage.

---
+++ producer-consumer-storage/src/problem/ConsumerWorker.java
@@ -0,0 +1,42 @@
+package problem;
+
+import java.util.function.Consumer;
+
+public class ConsumerWorker<T> implements Runnable {
+	
+	private Storage<T> storage;
+	private Consumer<T> consumer;
+	private boolean debug;
+	
+	public ConsumerWorker(Storage<T> storage, Consumer<T> consumer, boolean debug) {
+		this.storage = storage;
+		this.consumer = consumer;
+		this.debug = debug;
+	}
+	
+	public ConsumerWorker(Storage<T> storage, Consumer<T> consumer) {
+		this(storage, consumer, false);
+	}
+
+	@Override
+	public void run() {
+		while (storage.isRunning()) {
+			try {
+				storage.waitWhenIsEmpty();
+				T data = storage.take();
+				if (debug) {
+					System.out.println(Thread.currentThread().getName() + " consumed " + data);
+				}
+				storage.notifyAllIsNoLongerFull();
+				consumer.accept(data);
+			} catch (InterruptedException e) {
+				if (debug) {
+					e.printStackTrace();
+				}
+				break;
+			}
+			
+		}
+	}
+
+}
