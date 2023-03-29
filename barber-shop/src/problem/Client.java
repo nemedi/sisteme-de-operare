8cf13d10bc947bdb3ae2b1b7d1b57be47a3ccf4b
Iulian Ilie Nemedi <iilie@axway.com>
2023-03-29 12:30:30 +0300

    INT - Updates.

--- barber-shop/src/problem/Client.java
+++ barber-shop/src/problem/Client.java
@@ -8,14 +8,14 @@ public class Client implements Runnable {
 	private FreeChairs freeChairs;
 	private Semaphore clients;
 	private Semaphore barberReady;
-	private Semaphore mutex;
+	private Semaphore chair;
 	
 	public Client(int id, FreeChairs freeChairs, Semaphore clients, Semaphore barberReady, Semaphore mutex) {
 		this.id = id;
 		this.freeChairs = freeChairs;
 		this.clients = clients;
 		this.barberReady = barberReady;
-		this.mutex = mutex;
+		this.chair = mutex;
 	}
 	
 	public int getId() {
@@ -27,18 +27,18 @@ public class Client implements Runnable {
 		while (true) {
 			try {
 				Thread.sleep(500);
-				mutex.acquire();
+				chair.acquire();
 				System.out.println("Client " + id + " is searching for a free chiar.");
 				if (freeChairs.getCount() > 0) {
 				    freeChairs.decrement();
 				    System.out.println("Client " + id + " found a free chair.");
 				    clients.release();
-				    mutex.release();
+				    chair.release();
 				    barberReady.acquire();
 				    System.out.println("Client " + id + " awaits to get a haircut.");
 				} else {
 					System.out.println("There are no free chairs.");
-				    mutex.release();
+				    chair.release();
 				    System.out.println("Client " + id + " leaves without getting a haircut.");
 				}
 			} catch (InterruptedException e) {
