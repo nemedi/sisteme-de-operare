3e7b6068cb44ca0efa84c42f67e0563338dd4a6c
Iulian Ilie Nemedi <iilie@axway.com>
2023-03-30 01:56:40 +0300

    INT - Updates.

---
+++ readers-writers/src/problem/Counter.java
@@ -0,0 +1,18 @@
+package problem;
+
+public class Counter {
+
+	private int value = 0;
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void increment() {
+		value++;
+	}
+	
+	public void decrement() {
+		value--;
+	}
+}
