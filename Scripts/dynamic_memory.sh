38358f2e20cbe0f568be0af6a08e88cab2d0afc4
Iulian Ilie Nemedi <iilie@axway.com>
2024-03-05 21:55:31 +0200

    INT - Scripts

---
+++ Scripts/dynamic_memory.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Afiseaza dimensiunea totala a zonelor de memorie
+# alocate dinamic de procesele care ruleaza un program
+# dat ca argument.
+if test "$#" -eq 1
+then
+    if test -f "$1" -o -f "$(which "$1")"
+    then
+        total=0
+        for pid in $(pidof "$1")
+        do
+            total=$(pmap "$pid" | head -n -1 | tail -n +2 | tr -s ' ' | (while read line
+            do
+                if test "$(echo "$line" | cut -d ' ' -f 4-)" = "[ anon ]"
+                then
+                    size=$(echo "$line" | cut -d ' ' -f 2 | cut -d K -f 1)
+                    total=$(expr "$total" + "$size")
+                fi
+            done
+            echo "$total"))
+        done
+        echo "Total amount of dynamic memory allocated to processes running '$1' is: ${total}K"
+    else
+        echo "File '$1' not found."
+        exit 2
+    fi
+else
+    echo "Usage: $0 <program>"
+    exit 1
+fi
